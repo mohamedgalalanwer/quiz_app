3de4bad04af8269831dc9d299bd923dab6fbe3c2
Mohamed Galal <mohamedjalalanwer@outlook.com>
2021-03-17 19:40:22 +0200

    upload

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Answer  extends StatelessWidget {
+class Answer extends StatelessWidget {
+  final String answerText;
+  final Function x;
+
+  Answer(this.x, this.answerText);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        onPressed: x,
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          answerText,
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'questions.dart';
+import 'answer.dart';
 
 main() => runApp(MyApp());
 
@@ -9,9 +10,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _questionIndex=0;
+  int _questionIndex = 0;
 
-  final _question=[
+  void answerQuestion(){
+    if(_questionIndex==1){
+       _questionIndex=-1;
+    }
+    setState(() {
+      _questionIndex+=1;
+    });
+   // print(_questionIndex);
+  //  print("g a l al  ");
+  }
+
+  final _question = [
     'What\'s your favorite color ? ',
     'What\'s your favorite animal ? '
   ];
@@ -19,14 +31,21 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Quiz App"),
-        ),
-        body: Container(
-        child: Question(_question[1]),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Quiz App"),
+            ),
+            body: Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Question(_question[_questionIndex]),
+                  Answer(answerQuestion, "Red"),
+                  Answer(answerQuestion, "Green"),
+                  Answer(answerQuestion, "Blue"),
+                  Answer(answerQuestion, "Black"),
+                ],
+              ),
+            )));
   }
 }
